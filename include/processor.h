045664525c9a5c6eb305cf2c29748797b682748d
aydenlin <463551449@qq.com>
2016-05-19 07:06:44 +0000

    processor commit.

---
+++ include/processor.h
@@ -0,0 +1,34 @@
+#ifndef _PROCESSOR_H_
+#define _PROCESSOR_H_
+
+#include "types.h"
+#include "network.h"
+#include "location.h"
+#include "certification.h"
+
+typedef struct processor Processor;
+
+typedef byte * Processor_work_obtain(Network *network);
+typedef void Certification_info_saving(Certification_info *C, 
+		Certifi_struct *cs);
+typedef void Location_info_saving(Location *L, Location_struct *ls);
+typedef void Control_message_gene(Network *network, int flag);
+typedef void Processing(byte *packet);
+
+byte * processor_work_obtain(Network *network);
+void certification_info_saving(Certification_info *C, 
+	 Certifi_struct *cs);
+void location_info_saving(Location *L, Location_struct *ls);
+void control_message_gene(Network *network, int flag);
+void processing(byte *packet);
+
+typedef struct processor {
+	byte *packet;
+	Processor_work_obtain *processor_work_obtain(Network *network);
+	Certification_info_saving *certification_info_saving;
+	Location_info_saving *location_info_saving;
+	Control_message_gene *control_message_gene;
+	Processing *processing;
+} Processor;
+
+#endif /* _PROCESSOR_H_ */
